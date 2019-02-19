d4bf64da2b4d4fe9dcfc50d4085658e6d02c07d6
DhrumilMistry <35608987+DhrumilMistry@users.noreply.github.com>
2019-02-19 17:05:58 -0500

    Modified MainActivity. New SignUpActivity Added. Temporary Splashscreen in place.

---
+++ app/src/main/java/com/example/knowfoodapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.knowfoodapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    public static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent showSignUpIntent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(showSignUpIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
